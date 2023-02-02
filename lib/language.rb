f66e2b602f77ef7d8968bb887c0ecf6f837e3c3b
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-02-02 11:26:29 +0000

    [edit]reviewer_comment_controller/reviewer_comment_show.html

--- lib/language.rb
+++ lib/language.rb
@@ -5,19 +5,23 @@ require 'net/https'
 module Language
   class << self
     def get_data(text)
-      api_url = "https://language.googleapis.com/v1/documents:analyzeSentiment?key=#{ENV['AIzaSyBOJvdxpruunPjcJTd1FJLujreMUYQlGQw']}"
+      # APIのURL作成
+      api_url = "https://language.googleapis.com/v1/documents:analyzeSentiment?key=#{ENV['GOOGLE_API_KEY']}"
+      # APIリクエスト用のJSONパラメータ
       params = {
         document: {
           type: 'PLAIN_TEXT',
           content: text
         }
       }.to_json
+      # Google Cloud Natural Language APIにリクエスト
       uri = URI.parse(api_url)
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
       request = Net::HTTP::Post.new(uri.request_uri)
       request['Content-Type'] = 'application/json'
       response = https.request(request, params)
+      # APIレスポンス出力
       response_body = JSON.parse(response.body)
       if (error = response_body['error']).present?
         raise error['message']
